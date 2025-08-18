a139a17d1954e547e7fd5774ccfb30c45c00c52b
roboticacesmac <roboticacesmac@gmail.com>
2025-08-18 11:44:01 -0300

    ajustando server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 // Projeto 1
 const app1 = express();
-const PORT1 = 3000;
+const PORT1 = 3001;
 app1.use(express.static(path.join(__dirname, "tournamenter-obr")));
 
 app1.listen(PORT1, () => {
@@ -12,9 +12,9 @@ app1.listen(PORT1, () => {
 
 // Projeto 2
 const app2 = express();
-const PORT2 = 3001;
-app2.use(express.static(path.join(__dirname, "build")));
+const PORT2 = 3002;
+app2.use(express.static(path.join(__dirname, "build"))); 
 
-app2.listen(PORT2, () => {
+app2.listen(PORT2, "0.0.0.0", () => {
   console.log(`Projeto 2 rodando em http://localhost:${PORT2}`);
 });
