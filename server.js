790082f52ac26488036f75b476e54c52f1025b93
roboticacesmac <roboticacesmac@gmail.com>
2025-08-18 17:23:03 -0300

    atualizando server

--- server.js
+++ server.js
@@ -4,10 +4,14 @@ const path = require("path");
 // Projeto 1
 const app1 = express();
 const PORT1 = 3001;
-app1.use(express.static(path.join(__dirname, "tournamenter-obr")));
 
-app1.listen(PORT1, () => {
-  console.log(`Projeto 1 rodando em http://localhost:${PORT1}`);
+app1.use(express.static(path.join(__dirname, "tournamenter-obr")));
+app1.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "tournamenter-obr", "index.html"));
+});
+
+app1.listen(PORT1, "0.0.0.0", () => {
+  console.log(`Projeto 1 rodando em http://0.0.0.0:${PORT1}`);
 });
 
 // Projeto 2
@@ -15,7 +19,6 @@ const app2 = express();
 const PORT2 = 3002;
 
 app2.use(express.static(path.join(__dirname, "build")));
-
 app2.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
