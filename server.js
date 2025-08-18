d739cd64320e64795ba5676bbe28cb4bcfd76d2e
roboticacesmac <roboticacesmac@gmail.com>
2025-08-18 11:05:08 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+// Projeto 1
+const app1 = express();
+const PORT1 = 3001;
+app1.use(express.static(path.join(__dirname, "tournamenter-obr")));
+
+app1.listen(PORT1, () => {
+  console.log(`Projeto 1 rodando em http://localhost:${PORT1}`);
+});
+
+// Projeto 2
+const app2 = express();
+const PORT2 = 3002;
+app2.use(express.static(path.join(__dirname, "build")));
+
+app2.listen(PORT2, () => {
+  console.log(`Projeto 2 rodando em http://localhost:${PORT2}`);
+});
