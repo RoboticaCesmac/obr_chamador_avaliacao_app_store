9642f1932268de9dc23ff6ec3d67df3a279fcfc3
roboticacesmac <roboticacesmac@gmail.com>
2025-08-18 17:20:38 -0300

    atualizando server

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ app1.listen(PORT1, () => {
 
 // Projeto 2
 const app2 = express();
-const PORT2 = 8008;
-app2.use(express.static(path.join(__dirname, "build"))); 
+const PORT2 = 3002;
+
+app2.use(express.static(path.join(__dirname, "build"))); // <-- adicione isso!
+
+app2.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app2.listen(PORT2, "0.0.0.0", () => {
-  console.log(`Projeto 2 rodando em http://localhost:${PORT2}`);
+  console.log("Projeto 2 rodando na porta 3002");
 });
