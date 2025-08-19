510212f0c840645ed7d2ec7e5314f9c2054f89f3
roboticacesmac <roboticacesmac@gmail.com>
2025-08-19 13:34:15 -0300

    corrigindo porta

--- server.js
+++ server.js
@@ -3,26 +3,18 @@ const path = require("path");
 
 // Projeto 1
 const app1 = express();
-const PORT1 = 3001;
-
+const PORT1 = 8001;
 app1.use(express.static(path.join(__dirname, "tournamenter-obr")));
-app1.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "tournamenter-obr", "index.html"));
-});
 
-app1.listen(PORT1, "0.0.0.0", () => {
-  console.log(`Projeto 1 rodando em http://0.0.0.0:${PORT1}`);
+app1.listen(PORT1, () => {
+  console.log(`Projeto 1 rodando em http://localhost:${PORT1}`);
 });
 
 // Projeto 2
 const app2 = express();
-const PORT2 = 3002;
-
-app2.use(express.static(path.join(__dirname, "build")));
-app2.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+const PORT2 = 8002;
+app2.use(express.static(path.join(__dirname, "build"))); 
 
 app2.listen(PORT2, "0.0.0.0", () => {
-  console.log(`Projeto 2 rodando em http://0.0.0.0:${PORT2}`);
+  console.log(`Projeto 2 rodando em http://localhost:${PORT2}`);
 });
